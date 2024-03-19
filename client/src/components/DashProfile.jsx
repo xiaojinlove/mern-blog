334fadf53e7f7205575e650201fd9ad57d5cbc88
gaoyan <magaoyanxj@gmail.com>
2024-03-19 20:02:27 +0800

    complete create a post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -21,9 +21,10 @@ import {
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi'
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -226,10 +227,24 @@ export default function DashProfile() {
           defaultValue={currentUser.email} onChange={handleChange}
         />
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-            
-          Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? 'loading...' : 'Update'}
         </Button>
+        {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'}>
+              <Button
+                type='button'
+                gradientDuoTone='purpleToPink'
+                className='w-full'
+              >
+                Create a post
+              </Button>
+            </Link>
+          )
+        }
       </form>
       
       <div className='text-red-500 flex justify-between mt-5'>
