dce84bad8d7c14287137f691393189f58fb8116d
gaoyan <magaoyanxj@gmail.com>
2024-03-20 21:57:32 +0800

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,28 +11,30 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import CreatePost from "./pages/CreatePost"
 import UpdatePost from './pages/UpdatePost'
-import PostPage from "./components/PostPage"
+import PostPage from "./pages/PostPage"
+import ScrollToTop from "./components/ScrollToTop"
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<PrivateRoute />} >
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        <Route element={<OnlyAdminPrivateRoute />} >
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
-        </Route>
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/post/:postSlug" element={<PostPage />} />
-      </Routes>
-    <FooterCom />
+      <ScrollToTop />
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />} >
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route element={<OnlyAdminPrivateRoute />} >
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path='/update-post/:postId' element={<UpdatePost />} />
+          </Route>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/post/:postSlug" element={<PostPage />} />
+        </Routes>
+      <FooterCom />
     </BrowserRouter>
   )
 }
